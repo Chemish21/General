994a2fd912d32040d7339e6194a44e31286c0ca5
Chemish21 <smorris3131@gmail.com>
2025-08-23 18:53:37 -0400

    Update lowLetC.c
    Updating to have proper version, last version was not current.

--- lowLetC.c
+++ lowLetC.c
@@ -13,20 +13,30 @@ int main(){
   //Array to hold comparison string
   char string[11];
 
-  //Getting string from user
-  printf("String limited to 10 characters\n");
-  printf("Characters past 10 and capital letters are not read\n");
+  //Informing user as well as getting input
+  printf("This program adds lowercase letters from a string by 1-26 value\n");
+  printf("Only accepts up to 10 lowercase chars\n");
+  printf("Uppercase letters/other chars are not counted\n");
+  printf("---------------------------------------------------\n");
+  printf("Reference: a-1, b-2, c-3, d-4, e-5, f-6, g-7\n");
+  printf("Continued: h-8, i-9, j-10, k-11, l-12, m-13\n");
+  printf("Continued: n-14, o-15, p-16, q-17, r-18, s-19\n");
+  printf("Continued: t-20, u-21, v-22, w-23, x-24, y-25, z-26\n");
   printf("---------------------------------------------------\n");
   printf("Enter String: ");
   scanf("%10s", string);
+  printf("\n");
 
+  printf("Calculating\n");
+  printf("-----------\n");
   //Index for string check
   int i = 0;
   //Loop through string
   while (string[i] != '\n' && string[i] != '\0') {
-    //When string char matches a lowercase char, add to total using numeric value of letter
+    //When string char matches a lowercase char, add to total using numeric value of char
     for (int j = 1; j <= 26; j++) { 
       if (string[i] == lowerC[j]) {
+        printf("Adding: %d + Letter %c: %d\n", total, string[i], numbC[j]);
         total += numbC[j];
         break; //End search after find
       }
